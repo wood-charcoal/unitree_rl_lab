736f9a9bce0d9e7b08526b003411d85469338f89
wood-charcoal <wuyurui@whu.edu.cn>
2025-07-07 17:36:41 +0800

    modify max iteration

--- train_h1.sh
+++ train_h1.sh
@@ -4,4 +4,4 @@ source ~/.bashrc
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate env_isaaclab
 
-python scripts/rsl_rl/train.py --headless --task Unitree-H1-Velocity --max_iterations 10000
+python scripts/rsl_rl/train.py --headless --task Unitree-H1-Velocity --max_iterations 20000
