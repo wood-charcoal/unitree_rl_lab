685a2047b14fd837935041fc4ba3c18b1b542ea8
wood-charcoal <wuyurui@whu.edu.cn>
2025-07-07 17:29:29 +0800

    add max iteration limit

--- train_go2.sh
+++ train_go2.sh
@@ -4,4 +4,4 @@ source ~/.bashrc
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate env_isaaclab
 
-HYDRA_FULL_ERROR=1 python scripts/rsl_rl/train.py --headless --task Unitree-Go2-Velocity
+HYDRA_FULL_ERROR=1 python scripts/rsl_rl/train.py --headless --task Unitree-Go2-Velocity --max_iterations 10000
--- train_lite0430.sh
+++ train_lite0430.sh
@@ -4,4 +4,4 @@ source ~/.bashrc
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate env_isaaclab
 
-HYDRA_FULL_ERROR=1 python scripts/rsl_rl/train.py --headless --task XHumanoid-Lite0430-Velocity
+python scripts/rsl_rl/train.py --headless --task XHumanoid-Lite0430-Velocity --max_iterations 20000
